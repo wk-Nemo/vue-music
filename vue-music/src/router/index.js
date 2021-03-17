789bfbbd355e07b266b9d8d82f72d80b8b83a83c
wk-Nemo <2181544173@qq.com>
2021-03-17 22:20:23 +0800

    header and tab

--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Recommend from '@/components/recommend/recommend'
+import Singer from '@/components/singer/singer'
+import Search from '@/components/search/search'
+import Rank from '@/components/rank/rank'
 
 Vue.use(Router)
 
@@ -8,8 +11,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      redirect: '/Recommend'
+    },
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/rank',
+      component: Rank
     }
   ]
 })
